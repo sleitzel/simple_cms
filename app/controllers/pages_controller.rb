fd768d9057667b99bc6af79a64b9b103f48d9e62
sleitzel <sleitzel06@gmail.com>
2016-05-03 17:27:41 -0500

    Added controllers

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,63 @@
+class PagesController < ApplicationController
+  
+  def index
+    @page = Page.sorted
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new({:name => "default"})
+  end
+
+  def create
+    #Instantiate a new object using form parameters
+    @page = Page.new(page_params)
+    #Save the Page
+    if @page.save
+      flash[:notice] = "Page has been created successfully"
+      # If the save succeeds, redirect to the index
+      redirect_to(:action => 'index')
+    else
+      # If the save fails, redisplay the form so the user can fix mistakes
+      render('new')
+    end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update_attributes(page_params)
+      flash[:notice] = "Page updated successfully."
+      redirect_to(:action => 'show')
+    else 
+      render('edit')
+    end
+  end
+
+  def delete
+    @page = Page.find(params[:id])
+  end
+
+  def destroy
+    page = Page.find(params[:id]).destroy
+    flash[:notice] = "Page deleted successfully."
+    redirect_to(:action => 'index')
+  end
+
+  private
+
+  def page_params
+    # same as using "params[:subject]", except that it:
+    # - raises an error if :subject is not present
+    # - allows listed attributes to be mass-assigned
+    params.require(:subject).permit(:name, :position, :visible)
+  end
+
+
+end
---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,71 @@
+class SubjectsController < ApplicationController
+
+  layout false
+
+  def index
+    @subjects = Subject.sorted
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def new
+    @subject = Subject.new({:name => "Default"})
+  end
+
+  def create
+    #Instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+    #Save the object
+    if @subject.save
+      flash[:notice] = "Subject created succesfully."
+      #If the save succeeds, reditect to the index action
+      redirect_to(:action => 'index')
+    else
+      #If the save fails, redisplay the form so the user can fix problems
+      render('new')
+    end
+  end
+
+  def edit
+
+    @subject = Subject.find(params[:id])
+
+  end
+
+  def update
+    # Find an existing object using form parameters
+    @subject = Subject.find(params[:id])
+    #Update the object
+    if @subject.update_attributes(subject_params) 
+    flash[:notice] = "Subject updated succesfully."
+      #If the supdate succeeds, reditect to the index action
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      #If the update fails, redisplay the form so the user can fix problems
+      render('edit')
+    end
+  end
+
+  def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject '#{subject.name}' deleted succesfully."
+    redirect_to(:action => 'index')
+
+  end
+
+  private
+
+  def subject_params
+    # same as using "params[:subject]", except that it:
+    # - taises an error if :subject is not present
+    # - allows listed attributes to be mass-assigned
+    params.require(:subject).permit(:name, :position, :visible)
+  end
+
+end
